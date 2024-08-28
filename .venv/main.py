1218a4fefb78bd06d120b96f9623bf500b3b8da1
Angel Reyes <areyes89@uabc.edu.mx>
2024-08-27 21:47:51 -0700

    Enlace a la base de datos

--- .venv/main.py
+++ .venv/main.py
@@ -1,20 +1,21 @@
 from flask import Flask
 from flask import render_template
 from flask import Flask, render_template, request, redirect, url_for, flash
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from datetime import datetime
-import bcrypt
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-DATABASE_URL = "postgresql+psycopg2://postgres:prueba123@localhost/web"
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
+#Base de datos-----------------------------------------------------------------
+DATABASE_URL = "postgresql://postgres:1234@localhost:5432/postgres"
 
+app.config['SQLALCHEMY_BINDS'] = {
+    'postgres': 'postgresql://postgres:1234@localhost:5432/postgres',
+}
+
+db = SQLAlchemy(app)
+
+#Rutas--------------------------------------------------------------------------
 @app.route("/")
 def index(name=None):
     return render_template('index.html')
@@ -35,5 +36,7 @@ def actualizar(name=None):
 def borrar(name=None):
     return render_template('borrar.html')
 
+
+#Inicializar el servidor -------------------------------------------------------
 if __name__ == "__main__":
     app.run(debug=True)
